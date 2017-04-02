eefe6bd1412d2c4e2524baf56eb077e9749d6c6c
Rishith Reddy <babarishith@gmail.com>
2017-04-02 21:01:56 +0530

    segmented into sentences and pre-processing

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,9 @@
 import sys, string
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import *
+stemmer = PorterStemmer()
 
 #load all review texts
 reviews = []
@@ -10,14 +12,32 @@ f = open(file,'r')
 for line in f:
 	l = line.strip().split('>')
 	if l[0] == '<Content':
-		#words to lower case
-		s = l[1].lower()
-		#remove punctuations and stopwords
-		s = s.translate(None, string.punctuation)
-		stop_words	 = set(stopwords.words('english'))
-		word_tokens = word_tokenize(s)
-		s = [w for w in word_tokens if not w in stop_words]
-		#Porter Stemmer
-		stemmed = [stemmer.stem(s) for w in s]
-		reviews.append(' '.join(stemmed))
-print len(reviews), reviews[1]
+		reviews.append(l[1])
+# print len(reviews), reviews[1]
+
+def parse_to_sentence(reviews):
+	sent = []
+	for r in reviews:
+		sentences = nltk.sent_tokenize(r)
+		for s in sentences:
+			#words to lower case
+			s = s.lower()
+			#remove punctuations and stopwords
+			s = s.translate(None, string.punctuation)
+			stop_words	 = [stemmer.stem(v) for v in stopwords.words('english')]
+			additional_stopwords = ["'s","...","'ve","``","''","'m",'--',"'ll","'d"]
+			stop_words = set(stop_words + additional_stopwords)
+			# print stop_words
+			# sys.exit()
+			word_tokens = word_tokenize(s)
+			s = [w for w in word_tokens if not w in stop_words]
+			#Porter Stemmer
+			stemmed = [stemmer.stem(w) for w in s]
+			if len(stemmed)>0:
+				sent.append(' '.join(stemmed))
+	return sent
+
+sent = parse_to_sentence(reviews)
+# print len(sent), sent[2]
+
+
