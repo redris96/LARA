454eaf199c78376f30cc9d29f3880e9edddc17ea
Mohammed Sharfuddin <mohammed.sharfuddinbit@gmail.com>
2017-04-28 23:02:36 +0530

    Updated LRR

--- regression.py
+++ regression.py
@@ -1,29 +1,47 @@
 import tensorflow as tf
 import numpy as np
+
 D = 2
+k = 4
+
 alpha_d = [0 for i in range(D)]
 multiplier = [0 for i in range(D)]
 obj_func = [0 for i in range(D)]
 loss = [0 for i in range(D)]
 train_op = [0 for i in range(D)]
 ratings = tf.constant([1.,2.],shape = [1,D])
-sigma_matr = tf.Variable(tf.diag(tf.ones(4)))
-delta = tf.Variable(0.2)
-for i in range(D):
-	alpha_d[i] = tf.Variable(tf.random_uniform([4,1],0,1))
-	x[i] = tf.multiply(tf.pow(tf.multiply(2.,tf.square(delta)),-1),
-		tf.subtract(ratings[i],tf.square(tf.reduce_sum(tf.matmul(tf.transpose(alpha_d[i]),alpha_d[i])))))
-	y[i] = tf.reduce_sum(tf.matmul(tf.transpose(alpha_d[i]),tf.matmul(sigma_matr,alpha_d[i])))
+x = [0 for i in range(D)]
+y = [0 for i in range(D)]
 
-	obj_func[i] = tf.add(x[i]+y[i])
-	loss[i] = obj_func[i]+tf.square(1-tf.reduce_sum(alpha_d[i]))
+sigma_matr = tf.placeholder(shape = [k,k],dtype = tf.float32)
+delta = tf.placeholder(dtype=tf.float32)
+mu = tf.placeholder(shape=[k,1],dtype=tf.float32)
+
+for i in range(D):
+	alpha_d[i] = tf.Variable(tf.random_uniform([k,1],0,1))
+	x[i] = tf.mul(tf.pow(tf.mul(2.,tf.square(delta)),-1),
+		tf.sub(ratings[i],tf.square(tf.reduce_sum(tf.matmul(tf.transpose(alpha_d[i]),alpha_d[i])))))
+	y[i] = 0.5*tf.reduce_sum(tf.matmul(tf.transpose(tf.sub(alpha_d[i],mu)),
+		tf.matmul(tf.matrix_inverse(sigma_matr),tf.sub(alpha_d[i],mu))))
+
+	obj_func[i] = tf.add(x[i],y[i])
+	loss[i] = tf.add(0.001*obj_func[i],tf.square(1-tf.reduce_sum(alpha_d[i])))
 	train_op[i] = tf.train.GradientDescentOptimizer(0.01).minimize(loss[i])
 
 init = tf.initialize_all_variables()
 
+act_alpha = [ [] for i in range(D)]
+act_sigma_matr = np.identity(k)
+act_delta = 0.2
+act_mu = np.random.rand(k,1)
+
 with tf.Session() as sess:
 	sess.run(init)
-	for i in range(2000):
+	for _ in range(3):
 		for j in range(D):
-			sess.run(train_op[j])
-	print(sess.run(alpha_d))
+			for i in range(80):
+				x,_ = sess.run([alpha_d[j],train_op[j]],feed_dict={sigma_matr:act_sigma_matr,
+					mu:act_mu,delta:act_delta})
+			act_alpha[j]=x
+
+	print(act_alpha)
